7b6ad8a7bf535d580ea968098dd5c6923a812561
yuu123456 <yuukunn3777@gmail.com>
2023-02-12 09:12:37 +0900

    Initial Commit

---
+++ SampleFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFirebase
+//
+//  Created by 秋山悠 on 2023/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
